42b42f7e312b91ad4c2d36411397c7e713664092
Fsanchess <112471380+Fsanchess@users.noreply.github.com>
2022-12-09 16:25:12 +0100

    homicide traitement + export

--- Traitement_Florent.py
+++ Traitement_Florent.py
@@ -5,8 +5,10 @@ crimes = pd.read_excel(path)
 crimes.columns = crimes.iloc[1]
 crimes = crimes[2:]
 crimes = crimes[(crimes.Year >= 2000) & (crimes.Year <= 2020)]
-crimes = crimes[(crimes.Indicator == 'Victims of intentional homicide') & (crimes.Dimension == 'Total')]
-
+crimes = crimes[(crimes.Indicator == 'Victims of intentional homicide') &
+                (crimes.Dimension == 'Total') & (crimes.Category == 'Total')
+                & (crimes.Sex == 'Total') & (crimes.Age == 'Total')]
+crimes = crimes.dropna()
 print(crimes)
 
 crimes.to_csv('dataset_clean/homicide_clean.csv', index=False)
