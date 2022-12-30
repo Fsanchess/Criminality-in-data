bb408499b5413dff569eeaba949d9bc769ab4151
Joelleuh <joelle.sabourdy@ynov.com>
2022-12-30 18:07:37 +0100

    modif taille texte

--- dashboard.py
+++ dashboard.py
@@ -35,6 +35,7 @@ choropleth_map = px.choropleth(plotting_choropleth_map,
                                color_continuous_scale="YlOrRd")
 
 choropleth_map.update_layout(
+    title = 'Criminalité par pays',
     height=700,
     geo=dict(
         showframe=True,
@@ -55,7 +56,7 @@ choropleth_map.update_layout(
 plt.figure(figsize=(7,3))
 
 # Display choropleth map
-title_choropleth_map = st.header("Nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
+title_choropleth_map = st.header("Map avec nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
 plot_choropleth_map = st.plotly_chart(choropleth_map, use_container_width=True)
 
 
@@ -65,16 +66,17 @@ criminalite = px.scatter(pd1_homicide, x="Country", y="VALUE",marginal_x= False,
                   labels={
                      "VALUE": "Nb crimes pour 100 000 habitants",
                      "Country": "Pays"},
+                  title = 'Criminalité par pays',
                   color_continuous_scale=["lightyellow", "orange", "red"]
                  )
 
 # Display charts with unemployment dataset
-title_unemployment_chart = st.header("Criminalité par pays")
+title_unemployment_chart = st.header("Nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
 plot_unemployment_chart =st.plotly_chart(criminalite, use_container_width=True)
 
 
 
-st.markdown("<p style= color: dark grey;'><br><br>Pour la suite de l'analyse, 6 pays avec des caractéristiques différents ont été choisis (développés/non développés, nord/sud, de tous les continents) :<br><ul><li>Australia (AUS)</li><li>Brazil (BRA)</li><li>India (IND)</li><li>Sweden (SWE)</li><li>United States of America (USA)</li><li>South Africa (ZAF)</li></ul><br></p>", unsafe_allow_html=True)
+st.markdown("<p style= color: dark grey><font size='4'><br><br>Pour la suite de l'analyse, 6 pays avec des caractéristiques différents ont été choisis (développés/non développés, nord/sud, de tous les continents) :<br><ul><li>Australia (AUS)</li><li>Brazil (BRA)</li><li>India (IND)</li><li>Sweden (SWE)</li><li>United States of America (USA)</li><li>South Africa (ZAF)</li></ul><br></font></p>", unsafe_allow_html=True)
 
 
 
@@ -83,6 +85,7 @@ pd2_homicide = homicide_dataset[homicide_dataset['Iso3_code'].isin(['AUS', 'BRA'
 pd2_homicide = pd2_homicide.loc[pd2_homicide['Unit of measurement'] == 'Rate per 100,000 population']
 pd2_homicide = pd2_homicide[pd2_homicide['Year'].isin([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020])]
 homicide_chart = px.line(pd2_homicide, x="Year", y="VALUE", color="Iso3_code", markers=True,
+                         title = 'Crime par pays',
                          category_orders={"Iso3_code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
                          labels={
                             "Year" : "Année",
@@ -101,6 +104,7 @@ prison_dataset = pd.read_csv(prison_path, sep = ';', encoding = "ISO-8859-1")
 pd_prison = prison_dataset[prison_dataset['Iso3_code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
 
 prison_chart = px.line(pd_prison, x="Year", y="VALUE", color="Iso3_code", markers=True,
+                         title = 'Prisonniers par pays',
                          category_orders={"Iso3_code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
                          labels={
                             "Year" : "Année",
@@ -165,6 +169,7 @@ unemployment_dataset = pd.read_csv(unemployment_path, sep = ';')
 pd_unemployment = unemployment_dataset[unemployment_dataset['Country Code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
 
 unemployment_chart = px.line(pd_unemployment, x="Year", y="Value", color="Country Code", markers=True,
+                         title = 'Chômage par pays',
                          category_orders={"Country Code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
                          labels={
                             "Year" : "Année",
