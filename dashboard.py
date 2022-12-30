528f4c80d8e29672b18970ea1114029fa33fa5df
Joelleuh <joelle.sabourdy@ynov.com>
2022-12-30 16:12:07 +0100

    modification des labels

--- dashboard.py
+++ dashboard.py
@@ -16,8 +16,8 @@ confi = st.set_page_config(
       layout="wide")
 
 # Dashboard title
-title = st.title('Evolution de la criminalité par pays selon différents critères sociaux de 2000 à 2020')
-
+#title = st.title('Evolution de la criminalité par pays selon différents critères sociaux de 2000 à 2020')
+title = st.markdown("<h1 style='text-align: center; color: dark grey;'>Evolution de la criminalité par pays<br>selon différents critères sociaux de 2000 à 2020<br><br></h1>", unsafe_allow_html=True)
 
 # Choropleth map with homicide dataset
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
@@ -32,10 +32,10 @@ choropleth_map = px.choropleth(plotting_choropleth_map,
                                locations="iso_a3",
                                color="VALUE",
                                hover_name="Country",
-                               color_continuous_scale=["lightyellow", "orange", "red"])
+                               color_continuous_scale="YlOrRd")
 
 choropleth_map.update_layout(
-    title_text='Victims of intentional homicide by Country (2010)',
+    height=700,
     geo=dict(
         showframe=True,
         showcoastlines=False,
@@ -47,15 +47,19 @@ choropleth_map.update_layout(
         xref='paper',
         yref='paper',
         text='Source: <a href="https://dataunodc.un.org/dp-intentional-homicide-victims">\
-            United Nations</a>',
+            Organisation des Nations unies</a>',
         showarrow = False
     )]
 )
 
+plt.figure(figsize=(7,3))
+
 # Display choropleth map
-title_choropleth_map = st.subheader("Nombre de victimes d'homicides volontaires par pays en 2010 (nb pour 100 000)")
+title_choropleth_map = st.subheader("Nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
 plot_choropleth_map = st.plotly_chart(choropleth_map, use_container_width=True)
 
+
+st.markdown("<p style= color: dark grey;'>Pour la suite de l'analyse, 6 pays avec des caractéristiques différents ont été choisis (développés/non développés, nord/sud, de tous les continents) :<br><ul><li>Australia (AUS)</li><li>Brazil (BRA)</li><li>India (IND)</li><li>Sweden (SWE)</li><li>United States of America (USA)</li><li>South Africa (ZAF)</li></ul></p>", unsafe_allow_html=True)
 
 
 # Charts with education dataset
@@ -66,11 +70,23 @@ below_path = 'Dataset/education_below-upper-secondary_25-64.csv'
 below_datatset = pd.read_csv(below_path, sep = ',')
 pd_below = below_datatset[below_datatset['LOCATION'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
 
-tertiary_chart = px.line(pd_tertiary, x="TIME", y="Value", color="LOCATION", title="Diplôme d'étude supérieur", markers=True)
-below_chart = px.line(pd_below, x="TIME", y="Value", color="LOCATION", title="Diplôme inférieur au niveau bac", markers=True)
+tertiary_chart = px.line(pd_tertiary, x="TIME", y="Value", color="LOCATION", title="Taux de diplômés d'étude supérieur", markers=True,
+                         category_orders={"LOCATION": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
+                         labels={
+                            "TIME" : "Année",
+                            "Value" : "Taux de diplômés",
+                            "LOCATION" : "Pays"
+                         })
+below_chart = px.line(pd_below, x="TIME", y="Value", color="LOCATION", title="Taux de diplômés inférieur au niveau bac", markers=True,
+                         category_orders={"LOCATION": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
+                         labels={
+                            "TIME" : "Année",
+                            "Value" : "Taux de diplômés",
+                            "LOCATION" : "Pays"
+                         })
 
 # Display charts with education dataset
-title_education_chart = st.subheader("Pourcentage de la population ayant un diplôme d'étude supérieur VS inférieur au niveau bac parmi les 25-64 ans")
+title_education_chart = st.subheader("Evolution du taux de diplômés d'étude supérieur VS inférieur au niveau bac parmi la population 25-64 ans")
 
 col1, col2 = st.columns(2)
 
@@ -85,7 +101,13 @@ with col2:
 pd2_homicide = homicide_dataset[homicide_dataset['Iso3_code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF'])]
 pd2_homicide = pd2_homicide.loc[pd2_homicide['Unit of measurement'] == 'Rate per 100,000 population']
 pd2_homicide = pd2_homicide[pd2_homicide['Year'].isin([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020])]
-homicide_chart = px.line(pd2_homicide, x="Year", y="VALUE", color="Country", title="taux crime", markers=True)
+homicide_chart = px.line(pd2_homicide, x="Year", y="VALUE", color="Iso3_code", markers=True,
+                         category_orders={"Iso3_code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
+                         labels={
+                            "Year" : "Année",
+                            "VALUE" : "Taux de criminalité",
+                            "Iso3_code" : "Pays"
+                         })
 
 # Display chart with homicide dataset
 title_homicide_chart = st.subheader('Evolution du taux de criminalité')
