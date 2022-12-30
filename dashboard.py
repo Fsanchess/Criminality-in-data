9ca3125c5b7f5d94e3638deb583c236290bec192
Joelleuh <joelle.sabourdy@ynov.com>
2022-12-30 13:38:44 +0100

    commentaire du code dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,38 +1,40 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.figure_factory as ff
-import matplotlib
-import matplotlib.pyplot as plt
+# Import
 import geopandas as gpd
 import mapclassify as mc
-
-st.set_page_config(
-    page_title="Dashboard",
-    layout="wide",
-)
-
-st.title('Evolution de la criminalité par pays selon différents critères sociaux de 2000 à 2020')
-
-
-###
-
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import plotly.express as px
+import plotly.figure_factory as ff
+import streamlit as st
+
+
+# Configuration of the page title and size
+confi = st.set_page_config(
+      page_title="Dashboard",
+      layout="wide")
+
+# Dashboard title
+title = st.title('Evolution de la criminalité par pays selon différents critères sociaux de 2000 à 2020')
+
+
+# Choropleth map with homicide dataset
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-file_name3 = 'dataset_clean/homicide_clean.csv'
-crimesA = pd.read_csv(file_name3)
-crimes = crimesA.loc[crimesA['Unit of measurement'] == 'Rate per 100,000 population']
-crimes = crimes.loc[crimes['Year'] == 2010]
+homicide_datapath = 'dataset_clean/homicide_clean.csv'
+homicide_dataset = pd.read_csv(homicide_datapath)
+pd1_homicide = homicide_dataset.loc[homicide_dataset['Unit of measurement'] == 'Rate per 100,000 population']
+pd1_homicide = pd1_homicide.loc[pd1_homicide['Year'] == 2010]
 
-for_plotting2 = world.merge(crimes, left_on = 'iso_a3', right_on = 'Iso3_code')
+plotting_choropleth_map = world.merge(pd1_homicide, left_on = 'iso_a3', right_on = 'Iso3_code')
 
-fig = px.choropleth(for_plotting2, locations="iso_a3",
-                    color="VALUE", # lifeExp is a column of gapminder
-                    hover_name="Country", # column to add to hover information
-                    color_continuous_scale=["lightyellow", "orange", "red"])
+choropleth_map = px.choropleth(plotting_choropleth_map,
+                               locations="iso_a3",
+                               color="VALUE",
+                               hover_name="Country",
+                               color_continuous_scale=["lightyellow", "orange", "red"])
 
-
-fig.update_layout(
+choropleth_map.update_layout(
     title_text='Victims of intentional homicide by Country (2010)',
     geo=dict(
         showframe=True,
@@ -50,47 +52,41 @@ fig.update_layout(
     )]
 )
 
-
-st.subheader('Nombre de victimes de homicides volontaires par pays en 2010 (nb pour 100 000)')
-
-plt.axis("off")
-st.plotly_chart(fig, use_container_width=True)
+# Display choropleth map
+title_choropleth_map = st.subheader("Nombre de victimes d'homicides volontaires par pays en 2010 (nb pour 100 000)")
+plot_choropleth_map = st.plotly_chart(choropleth_map, use_container_width=True)
 
 
-###
-st.subheader("Pourcentage de la population ayant un diplôme d'étude supérieur VS inférieur au niveau bac parmi les 25-64 ans")
 
-path_tertiary = 'Dataset/education_tertiary_25-64.csv'
-tertiary = pd.read_csv(path_tertiary, sep = ',')
-tertiary = tertiary[tertiary['LOCATION'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
-path_below = 'Dataset/education_below-upper-secondary_25-64.csv'
-below = pd.read_csv(path_below, sep = ',')
-below = below[below['LOCATION'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
+# Charts with education dataset
+tertiary_path = 'Dataset/education_tertiary_25-64.csv'
+tertiary_dataset = pd.read_csv(tertiary_path, sep = ',')
+pd_tertiary = tertiary_dataset[tertiary_dataset['LOCATION'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
+below_path = 'Dataset/education_below-upper-secondary_25-64.csv'
+below_datatset = pd.read_csv(below_path, sep = ',')
+pd_below = below_datatset[below_datatset['LOCATION'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
 
+tertiary_chart = px.line(pd_tertiary, x="TIME", y="Value", color="LOCATION", title="Diplôme d'étude supérieur", markers=True)
+below_chart = px.line(pd_below, x="TIME", y="Value", color="LOCATION", title="Diplôme inférieur au niveau bac", markers=True)
 
-fig2 = px.line(tertiary, x="TIME", y="Value", color="LOCATION", title="Diplôme d'étude supérieur", markers=True)
-
-fig3 = px.line(below, x="TIME", y="Value", color="LOCATION", title="Diplôme inférieur au niveau bac", markers=True)
-
+# Display charts with education dataset
+title_education_chart = st.subheader("Pourcentage de la population ayant un diplôme d'étude supérieur VS inférieur au niveau bac parmi les 25-64 ans")
 
 col1, col2 = st.columns(2)
 
 with col1:
-   st.plotly_chart(fig2, use_container_width=True)
-
+   st.plotly_chart(tertiary_chart, use_container_width=True)
 with col2:
-   st.plotly_chart(fig3, use_container_width=True)
+   st.plotly_chart(below_chart, use_container_width=True)
 
 
 
+# Chart with homicide dataset
+pd2_homicide = homicide_dataset[homicide_dataset['Iso3_code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF'])]
+pd2_homicide = pd2_homicide.loc[pd2_homicide['Unit of measurement'] == 'Rate per 100,000 population']
+pd2_homicide = pd2_homicide[pd2_homicide['Year'].isin([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020])]
+homicide_chart = px.line(pd2_homicide, x="Year", y="VALUE", color="Country", title="taux crime", markers=True)
 
-
-###
-st.subheader('Evolution du taux de criminalité')
-
-evol_crime = crimesA[crimesA['Iso3_code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF'])]
-evol_crime = evol_crime.loc[evol_crime['Unit of measurement'] == 'Rate per 100,000 population']
-evol_crime = evol_crime[evol_crime['Year'].isin([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020])]
-fig5 = px.line(evol_crime, x="Year", y="VALUE", color="Country", title="taux crime", markers=True)
-
-st.plotly_chart(fig5, use_container_width=True)
+# Display chart with homicide dataset
+title_homicide_chart = st.subheader('Evolution du taux de criminalité')
+plot_homicide_chart =st.plotly_chart(homicide_chart, use_container_width=True)
