2e97a04ffdf35ff4fe210e77938b775541ef7af0
Joelleuh <joelle.sabourdy@ynov.com>
2023-01-04 19:47:02 +0100

    déplacement commentaire

--- dashboard.py
+++ dashboard.py
@@ -86,9 +86,7 @@ st.markdown("<p style= color: dark grey><font size='4'><br>Le Honduras est le pa
 
 st.markdown("<p style= color: dark grey><font size='4'><br><br>Les nombres d'homicides volontaires pour 100 000 habitants varient considérablement d'un pays à l'autre. Bien qu'il y ait rarement une raison claire pour laquelle des crimes sont commis, de nombreux facteurs peuvent affecter les taux de criminalité.</font></p>", unsafe_allow_html=True)
 
-st.markdown("<p style= color: dark grey><font size='4'>Les pays ayant des taux de criminalité élevés ont généralement des niveaux de pauvreté élevés et une faible disponibilité d'emplois, des conditions susceptibles de forcer les gens à adopter des solutions plus risquées, plus désespérées et moralement discutables (qui sont souvent rendues possibles par des organismes d'application de la loi sous-développés). Les taux de criminalité ont tendance à être plus faibles dans les pays où les conditions de vie sont favorables (riches), l'application de la loi par la police et des peines plus sévères pour les crimes.</font></p>", unsafe_allow_html=True)
-
-st.markdown("<p style= color: dark grey><font size='4'><br>Ces taux de criminalité seront comparés avec divers paramètres sociaux et 6 pays avec des caractéristiques différents ont été choisis pour la suite de l'analyse (développés/non développés, nord/sud, de tous les continents) : Australia (AUS), Brazil (BRA), India (IND), Sweden (SWE), United States of America (USA), South Africa (ZAF).<br><br></font></p>", unsafe_allow_html=True)
+st.markdown("<p style= color: dark grey><font size='4'>Ces taux de criminalité seront comparés avec divers paramètres sociaux et 6 pays avec des caractéristiques différents ont été choisis pour la suite de l'analyse (développés/non développés, nord/sud, de tous les continents) : Australia (AUS), Brazil (BRA), India (IND), Sweden (SWE), United States of America (USA), South Africa (ZAF).<br><br></font></p>", unsafe_allow_html=True)
 
 
 
@@ -279,6 +277,10 @@ happiness_chart.update_layout(updatemenus=[go.layout.Updatemenu(active=0,buttons
 title_happiness_chart = st.header("Score de bonheur et satisfaction de vie, de 0 à 10")
 plot_happiness_chart =st.plotly_chart(happiness_chart, use_container_width=True)
 st.markdown("<p style= color: dark grey><font size='4'>Dans ce graphique représentant le bonheur et la satisfaction à l'égard de la vie autodéclarée par les habitants, nous pouvons voir que les habitants d'Inde et d'Afrique du Sud ne sont pas très heureux ce qui n'est pas forcement en accord avec les différents indicateurs tels que le taux de chômage ou le nombre de crime.<br><br><br></font></p>", unsafe_allow_html=True)
+
+
+
+st.markdown("<p style= color: dark grey><font size='4'>Les pays ayant des taux de criminalité élevés ont généralement des niveaux de pauvreté élevés et une faible disponibilité d'emplois, des conditions susceptibles de forcer les gens à adopter des solutions plus risquées, plus désespérées et moralement discutables (qui sont souvent rendues possibles par des organismes d'application de la loi sous-développés). Les taux de criminalité ont tendance à être plus faibles dans les pays où les conditions de vie sont favorables (riches), l'application de la loi par la police et des peines plus sévères pour les crimes.<br><br><br></font></p>", unsafe_allow_html=True)
 
 
 
