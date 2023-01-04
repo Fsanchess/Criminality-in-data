147055bea3c21e9d588b6e8911f8c835366c9f77
Joelleuh <joelle.sabourdy@ynov.com>
2023-01-04 13:53:43 +0100

    modif titre

--- dashboard.py
+++ dashboard.py
@@ -36,7 +36,7 @@ choropleth_map = px.choropleth(plotting_choropleth_map,
                                color_continuous_scale="YlOrRd")
 
 choropleth_map.update_layout(
-    title = 'Criminalité par pays',
+    title = 'Criminalité par pays (pour 100 000 habitants)',
     height=700,
     geo=dict(
         showframe=True,
@@ -57,7 +57,7 @@ choropleth_map.update_layout(
 plt.figure(figsize=(7,3))
 
 # Display choropleth map
-title_choropleth_map = st.header("Map avec nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
+title_choropleth_map = st.header("Map avec nombre de victimes d'homicide volontaire par pays en 2010")
 plot_choropleth_map = st.plotly_chart(choropleth_map, use_container_width=True)
 
 
@@ -70,12 +70,12 @@ criminalite = px.scatter(pd1_homicide, x="Country", y="VALUE",marginal_x= False,
                   labels={
                      "VALUE": "Nb crimes pour 100 000 habitants",
                      "Country": "Pays"},
-                  title = 'Criminalité par pays',
+                  title = 'Criminalité par pays (pour 100 000 habitants)',
                   color_continuous_scale=["lightyellow", "orange", "red"]
                  )
 
 # Display charts with unemployment dataset
-title_unemployment_chart = st.header("Nombre de victimes d'homicide volontaire par pays en 2010 (pour 100 000 habitants)")
+title_unemployment_chart = st.header("Nombre de victimes d'homicide volontaire par pays en 2010")
 plot_unemployment_chart =st.plotly_chart(criminalite, use_container_width=True)
 
 
@@ -93,16 +93,18 @@ pd2_homicide = homicide_dataset[homicide_dataset['Iso3_code'].isin(['AUS', 'BRA'
 pd2_homicide = pd2_homicide.loc[pd2_homicide['Unit of measurement'] == 'Rate per 100,000 population']
 pd2_homicide = pd2_homicide[pd2_homicide['Year'].isin([2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020])]
 homicide_chart = px.bar(pd2_homicide, x="Year", y="VALUE", color="Iso3_code",
-                         title = 'Crime par pays',
+                         title = 'Criminalité par pays (pour 100 000 habitants)',
                          category_orders={"Iso3_code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
                          labels={
                             "Year" : "Année",
                             "VALUE" : "Nb crimes pour 100 000 habitants",
                             "Iso3_code" : "Pays"
-                         })
+                         },
+                         height=550,
+                         )
 
 # Display chart with homicide dataset
-title_homicide_chart = st.header('Evolution du taux de criminalité')
+title_homicide_chart = st.header("Evolution du nombre de victimes d'homicide volontaire")
 plot_homicide_chart =st.plotly_chart(homicide_chart, use_container_width=True)
 
 
@@ -132,7 +134,7 @@ prison_dataset = pd.read_csv(prison_path, sep = ';', encoding = "ISO-8859-1")
 pd_prison = prison_dataset[prison_dataset['Iso3_code'].isin(['AUS', 'BRA', 'IND', 'SWE', 'USA', 'ZAF']) ]
 
 prison_chart = px.line(pd_prison, x="Year", y="VALUE", color="Iso3_code", markers=True,
-                         title = 'Prisonniers par pays',
+                         title = 'Prisonniers par pays (pour 100 000 habitants)',
                          category_orders={"Iso3_code": ["AUS", "BRA", "IND", "SWE","USA","ZAF"]},
                          labels={
                             "Year" : "Année",
